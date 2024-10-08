ed6605ceeeb020b35132f8d13e4d84422c6c1932
emisilva05 <165793297+emisilva05@users.noreply.github.com>
2024-10-08 08:44:56 -0300

    gcjgffgu

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'filme',
+    'Qual o nome do dono dos brinquedos no toy story 3?',
+    'Andy'
+)
+
+criaCartao(
+    'serie',
+    'Qual o primeiro personagem que morre em la casa de papel?',
+    'Radko Dragic'
+)
+
+criaCartao(
+    'desenho infantil',
+    'Qual o nome da melhor amiga do Peixonauta',
+    'Marina'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasilia'
+)
